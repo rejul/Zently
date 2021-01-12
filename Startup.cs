0840613e0b3fd90c4452a8f7c36d94228446c68d
unknown <rejul.reghu@gmail.com>
2021-01-12 14:46:08 +0530

    Commit message

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zently.Startup))]
+namespace Zently
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
